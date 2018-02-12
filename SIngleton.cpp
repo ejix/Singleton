06ec5df0e7d22f2fed6e7820d424f410505e9b25
ejix <bakeliasz@gmail.com>
2018-02-12 18:45:13 +0100

    Add files via upload

--- SIngleton.cpp
+++ SIngleton.cpp
@@ -2,10 +2,24 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
-
+class Singleton {
+private:
+	Singleton();
+public:
+	Singleton(Singleton const &copy) = delete;
+	Singleton&operator=(Singleton const&copy) = delete;
+	static Singleton& getInstance()
+	{
+		static Singleton instance;
+		return instance;
+	}
+};
 int main()
 {
+	 Singleton::getInstance();
+
     return 0;
 }
 
